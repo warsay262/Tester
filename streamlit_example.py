9a33e9593906785837ec076fcddc557292213b0f
Warsay Tekle <102491025+warsay262@users.noreply.github.com>
2023-07-26 22:28:30 -0400

    Update streamlit_example.py

--- streamlit_example.py
+++ streamlit_example.py
@@ -9,7 +9,7 @@ def load_data():
 
 def main():
     # Add a title to the app
-    st.title('USA Total Population by Year')
+    st.title('USA Population Comparison by State')
 
     # Load the data from the CSV file
     df = load_data()
@@ -19,23 +19,24 @@ def main():
     years = df['year'].unique()
 
     # Add dropdown widgets for state, year, and population type
-    selected_state = st.selectbox('Select State/Region:', states)
+    selected_state = st.selectbox('Select State:', states)
     selected_year = st.selectbox('Select Year:', years)
     selected_population_type = st.selectbox('Select Population Type:', ['total', 'under18'])
 
-    # Filter the dataset based on user selections
-    df_filtered = df[(df['state/region'] == selected_state) & (df['year'] == selected_year) & (df['ages'] == selected_population_type)]
+    # Filter the dataset for the 'USA' state/region and the selected year and population type
+    df_usa = df[(df['state/region'] == 'USA') & (df['year'] == selected_year) & (df['ages'] == selected_population_type)]
 
-    # Display the filtered data as a table
-    st.write('Filtered Data:')
-    st.write(df_filtered)
+    # Filter the dataset for the selected state/region and the selected year and population type
+    df_selected_state = df[(df['state/region'] == selected_state) & (df['year'] == selected_year) & (df['ages'] == selected_population_type)]
 
     # Create a bar chart using matplotlib and display it using Streamlit
     fig, ax = plt.subplots()
-    ax.bar(df_filtered['ages'], df_filtered['population'])
-    ax.set_xlabel('Population Type')
+    ax.bar('USA', df_usa['population'].values[0], label='USA')
+    ax.bar(selected_state, df_selected_state['population'].values[0], label=selected_state)
+    ax.set_xlabel('State/Region')
     ax.set_ylabel('Population')
-    ax.set_title(f'{selected_state} Population - Year {selected_year}')
+    ax.set_title(f'Comparison of {selected_state} Population with USA - Year {selected_year}')
+    ax.legend()
     st.pyplot(fig)
 
 if __name__ == '__main__':
