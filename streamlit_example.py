ad5a7c2492462ab1f0cfefa9bf66823f20154d73
Warsay Tekle <102491025+warsay262@users.noreply.github.com>
2023-07-26 23:59:08 -0400

    Update streamlit_example.py

--- streamlit_example.py
+++ streamlit_example.py
@@ -5,24 +5,41 @@ import matplotlib.pyplot as plt
 # Read the dataset
 df = pd.read_csv('state-population.csv')
 
-# Create a Streamlit app
-st.title('US State Population Dashboard')
+def main():
+    st.title('State Population Comparison')
+    st.sidebar.title('Select States/Regions')
+    
+    # Get a list of unique states/regions from the data
+    states = data['state/region'].unique().tolist()
+    
+    # Allow users to select multiple states/regions from the sidebar
+    selected_states = st.sidebar.multiselect('Select States/Regions', states)
+    
+    if not selected_states:
+        st.warning('Please select at least one state/region.')
+        return
+    
+    # Filter the data based on the selected states/regions
+    filtered_data = data[data['state/region'].isin(selected_states)]
+    
+    # Display the data table
+    st.write('Selected State/Region Population Data:')
+    st.dataframe(filtered_data)
+    
+    # Create and display the bar chart
+    fig, ax = plt.subplots()
+    for state in selected_states:
+        state_data = filtered_data[filtered_data['state/region'] == state]
+        ax.bar(state_data['year'], state_data['population'], label=state)
+    
+    plt.xlabel('Year')
+    plt.ylabel('Population')
+    plt.title('Population Comparison by State/Region')
+    plt.legend()
+    
+    st.write('Population Comparison Bar Chart:')
+    st.pyplot(fig)
 
-# Sidebar widgets for user interaction
-state = st.sidebar.selectbox('Select a state/region:', df['state/region'].unique())
-year = st.sidebar.selectbox('Select a year:', df['year'].unique())
-population_type = st.sidebar.radio('Choose population type:', ['total', 'under18'])
+if __name__ == '__main__':
+    main()
 
-# Filter the data based on user selection
-filtered_data = df[(df['state/region'] == state) & (df['year'] == year) & (df['ages'] == population_type)]
-
-# Display the selected data in a table
-st.table(filtered_data)
-
-# Create and display the bar chart
-plt.figure(figsize=(10, 6))
-plt.bar(filtered_data['ages'], filtered_data['population'])
-plt.xlabel('Population Type')
-plt.ylabel('Population')
-plt.title(f'{population_type.capitalize()} Population in {state} ({year})')
-st.pyplot(plt)
