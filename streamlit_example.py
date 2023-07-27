a339cc4de9077d73da061b555f2dda121b89cb9f
Warsay Tekle <102491025+warsay262@users.noreply.github.com>
2023-07-27 00:09:06 -0400

    Update streamlit_example.py

--- streamlit_example.py
+++ streamlit_example.py
@@ -2,15 +2,15 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Load the data
 @st.cache
 def load_data():
     df = pd.read_csv('state-population.csv')
     return df
 
-# Load the data from the CSV file
 df = load_data()
 
-# Add a title to the app
+# Create the Streamlit app components
 st.title('Interactive Population Dashboard')
 
 # Create a dropdown for state/region selection
@@ -19,14 +19,9 @@ selected_state = st.selectbox('Select a State/Region', df['state/region'].unique
 # Create a radio button for population type selection
 population_type = st.radio('Population Type', ['total', 'under18'])
 
-# Filter the dataset based on user selections
+# Filter the data based on user input
 filtered_data = df[(df['state/region'] == selected_state) & (df['ages'] == population_type)]
 
-# Display the filtered data as a table
-st.write('Filtered Data:')
-st.write(filtered_data)
-
-# Create a bar chart using matplotlib and display it using Streamlit
 # Plot the data as a line chart
 fig, ax = plt.subplots()
 ax.plot(filtered_data['year'], filtered_data['population'], marker='o', linestyle='-')
