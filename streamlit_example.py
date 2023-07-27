58e12060df3e3aa1165cb9a68fa7678684252ff6
Warsay Tekle <102491025+warsay262@users.noreply.github.com>
2023-07-27 00:14:04 -0400

    Update streamlit_example.py

--- streamlit_example.py
+++ streamlit_example.py
@@ -3,24 +3,40 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def load_data():
-    df = pd.read_csv('state-population.csv')
-    return df
+    # Load the dataset from the CSV file
+    data = pd.read_csv("state-population.csv")
+    return data
 
-df = st.cache_data(allow_output_mutation=True)(load_data)()
+def main():
+    # Add a title to the app
+    st.title('USA Total Population by Year')
 
-st.title('Interactive Population Dashboard')
+    # Load the data from the CSV file
+    df = load_data()
 
-# Create a dropdown for state/region selection
-selected_state = st.selectbox('Select a State/Region', df['state/region'].unique())
+    # Get unique states and years for the dropdowns
+    states = df['state/region'].unique()
+    years = df['year'].unique()
 
-# Create a radio button for population type selection
-population_type = st.radio('Population Type', ['total', 'under18'])
+    # Add dropdown widgets for state, year, and population type
+    selected_state = st.selectbox('Select State:', states)
+    selected_year = st.selectbox('Select Year:', years)
+    selected_population_type = st.selectbox('Select Population Type:', ['total', 'under18'])
 
-filtered_data = df[(df['state/region'] == selected_state) & (df['ages'] == population_type)]
+    # Filter the dataset based on user selections
+    df_filtered = df[(df['state/region'] == selected_state) & (df['year'] == selected_year) & (df['ages'] == selected_population_type)]
 
-fig, ax = plt.subplots()
-ax.bar(filtered_data['year'], filtered_data['population'])
-ax.set_xlabel('Year')
-ax.set_ylabel('Population')
-ax.set_title(f'{selected_state} - {population_type.capitalize()} Population from 1990 to 2012')
-st.pyplot(fig)
+    # Display the filtered data as a table
+    st.write('Filtered Data:')
+    st.write(df_filtered)
+
+    # Create a bar chart using matplotlib and display it using Streamlit
+    fig, ax = plt.subplots()
+    ax.bar(df_filtered['ages'], df_filtered['population'])
+    ax.set_xlabel('Population Type')
+    ax.set_ylabel('Population')
+    ax.set_title(f'{selected_state} Population - Year {selected_year}')
+    st.pyplot(fig)
+
+if __name__ == '__main__':
+    main()
