73e8dd17f1cdef4bfaa09ae835b950c9d713f62b
Warsay Tekle <102491025+warsay262@users.noreply.github.com>
2023-07-26 22:24:49 -0400

    Update streamlit_example.py

--- streamlit_example.py
+++ streamlit_example.py
@@ -14,15 +14,28 @@ def main():
     # Load the data from the CSV file
     df = load_data()
 
-    # Filter the dataset for the 'total' rows and the 'USA' state/region
-    df_usa = df[(df['state/region'] == 'USA') & (df['ages'] == 'total')]
+    # Get unique states and years for the dropdowns
+    states = df['state/region'].unique()
+    years = df['year'].unique()
+
+    # Add dropdown widgets for state, year, and population type
+    selected_state = st.selectbox('Select State:', states)
+    selected_year = st.selectbox('Select Year:', years)
+    selected_population_type = st.selectbox('Select Population Type:', ['total', 'under18'])
+
+    # Filter the dataset based on user selections
+    df_filtered = df[(df['state/region'] == selected_state) & (df['year'] == selected_year) & (df['ages'] == selected_population_type)]
+
+    # Display the filtered data as a table
+    st.write('Filtered Data:')
+    st.write(df_filtered)
 
     # Create a bar chart using matplotlib and display it using Streamlit
     fig, ax = plt.subplots()
-    ax.bar(df_usa['year'], df_usa['population'])
-    ax.set_xlabel('Year')
-    ax.set_ylabel('Total Population')
-    ax.set_title('USA Total Population by Year')
+    ax.bar(df_filtered['ages'], df_filtered['population'])
+    ax.set_xlabel('Population Type')
+    ax.set_ylabel('Population')
+    ax.set_title(f'{selected_state} Population - Year {selected_year}')
     st.pyplot(fig)
 
 if __name__ == '__main__':
